549d37fc78878169adad734afb382b2d990f1e7c
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-08-28 16:22:24 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+      apiOptions: {
+        region: "ca",
+      },
+    }),
+    tailwind()],
+    vite:{
+      plugins:[ basicSsl()],
+      server: {
+        https: true,
+        port:3000,
+      },
+    }
+});
